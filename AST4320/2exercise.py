aa0e18630889da7b9f715a5835a4335b072bc2cc
mrnafstad <mr.nafstad@gmail.com>
2016-09-21 19:34:36 +0200

    updates

--- AST4320/2exercise.py
+++ AST4320/2exercise.py
@@ -3,10 +3,19 @@ from numpy import *
 
 H0 = 70e3 				#km / s / Mpc
 n = 1e5
+G = 6.674e-11			#m^3 / kg / s^-2 7
+rho0 = 1
+
+
 
 def adotovera(massfrac, constfrac, a_i):
 	return H0*sqrt(massfrac/float(a**3) + constfrac)
 
+
 t = linspace(0, 1e6, n)
 logdelta = zeros(n)
 loga = zeros(n)
+
+loga[0] = -3
+logdelta[0] = -3
+
